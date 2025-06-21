a261a8d563acb97ccfac8f63faf7c02ce34d2e9f
R.K.Lakshan <lakshan2725@gmail.com>
2025-06-21 09:32:49 +0530

    [LS-MF-001]|Moble|Frontend-file structure setting

---
+++ assets/images/images.ts
@@ -0,0 +1,11 @@
+import logo from "@/assets/images/logo.png";
+import child from "@/assets/images/child.png";
+import whreeCareBegin from "@/assets/images/WhereCareBegins.png"
+import littlesteps from "@/assets/images/littlesteps.png"
+
+export const images={
+    logo,
+    child,
+    whreeCareBegin,
+    littlesteps
+}
