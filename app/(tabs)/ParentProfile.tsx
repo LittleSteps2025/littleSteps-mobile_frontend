a4c197e6444eb7959ff35cd0bbb34eb607353187
R.K.Lakshan <lakshan2725@gmail.com>
2025-07-03 12:06:55 +0530

    Update ParentProfile.tsx

--- app/(tabs)/ParentProfile.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-  Modal,
-  TextInput,
-  Alert
-} from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import { useRouter } from 'expo-router';
-    email: 'sarah.johnson@email.com',
-
