2d06e6cbb37a8a2967d0b27eed44dcba68a76c4e
Pramodi Peshila <ppeshila7@gmail.com>
2025-07-06 11:54:24 +0530

    LS-MF-024|Modified API url

--- utility/index.ts
+++ utility/index.ts
@@ -1,4 +1,7 @@
 // In your utility/index.ts file
 // export const API_BASE_URL = 'http://192.168.114.156:5001/api';
-export const API_BASE_URL = 'http://192.168.114.156:5001/api';
+// export const API_BASE_URL = 'http://192.168.114.156:5001/api';
 // or whatever your correct backend URL is
+
+//pramodi url
+export const API_BASE_URL = 'http://192.168.43.74:5001/api'
