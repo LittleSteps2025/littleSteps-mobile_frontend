8eaf0570cb43b0bc8ea5347f68afe40e5226c4f9
Pramodi Peshila <ppeshila7@gmail.com>
2025-07-17 11:34:51 +0530

    created signin route

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   useEffect(() => {
     const timer = setTimeout(() => {
       // Using router.push() to navigate to dashboard
-      router.replace('/teacher/signin'); // Change this to your actual dashboard route
+      router.replace('/signin'); // Change this to your actual dashboard route
       // or use router.replace('/dashboard') if you don't want user to go back
     },500); // 5000ms = 5 seconds
 
